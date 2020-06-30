6a4a12d3849d7b9cb8b84ca59851b4f86376b603
aokiti <tomohiroaoki1ban@icloud.com>
2020-06-30 21:32:48 +0900

    ffffff

---
+++ Quiz SavvyTests/Quiz_SavvyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Quiz_SavvyTests.swift
+//  Quiz SavvyTests
+//
+//  Created by 青木智大 on 2020/06/28.
+//  Copyright © 2020 青木智大. All rights reserved.
+//
+
+import XCTest
+@testable import Quiz_Savvy
+
+class Quiz_SavvyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
