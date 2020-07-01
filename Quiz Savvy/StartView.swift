18e901d55e09d58e26f3ac38a9e72386ff85bc06
aokiti <tomohiroaoki1ban@icloud.com>
2020-07-02 00:33:08 +0900

    knk.

--- Quiz Savvy/StartView.swift
+++ Quiz Savvy/StartView.swift
@@ -12,7 +12,7 @@ struct StartView: View {
     var body: some View {
         NavigationView {
             VStack(alignment: .center, spacing: 300.0){
-                Text("まんぴ")
+                Text("青梗菜")
                     .font(.largeTitle)
                     .fontWeight(.thin)
                     .padding()
