2a685bc891dd50c128b7b4b35b5050cd26d67a96
aokiti <tomohiroaoki1ban@icloud.com>
2020-07-26 15:34:26 +0900

    gk

--- Quiz Savvy/QuizDetailView.swift
+++ Quiz Savvy/QuizDetailView.swift
@@ -1,27 +1,91 @@
 //
-//  DetailView.swift
+//  QuizRow.swift
 //  Quiz Savvy
 //
-//  Created by 青木智大 on 2020/06/28.
+//  Created by 青木智大 on 2020/07/03.
 //  Copyright © 2020 青木智大. All rights reserved.
 //
 
 import SwiftUI
 
 struct QuizDetailView: View {
-
-    var num: String = "k"
+    @State var showingDetail1 = false
+    @State var showingDetail2 = false
+    @State var ans1: Bool = false
+    @State var ans2: Bool = false
+    @State var list: [String] = []
+    @State var showText = false
+    var now: Quiz
+    var next: Quiz
+    var i: Int = 0
+    var numA: Int = 0
     
     var body: some View {
-        ForEach(quizStore.quiz1.shuffled()) {q in
-            QuizRowView(q: q)
-        }
+        
+            VStack {
+                Text(now.title)
+                Text(now.explain)
+                Spacer()
+                Button(action: {
+                    self.list = []
+                    self.list.append(self.now.OKans)
+                    self.list.append(self.now.BADans)
+                    self.list.shuffle()
+                    self.showText.toggle()
+                }) {
+                    Text("result")
+                        .padding(.bottom)
+                        .frame(height: 200.0)
+                }
+                if showText {
+                    Button(action: {
+                        self.showingDetail1.toggle()
+                        if self.list[0] == self.now.OKans {
+                            self.ans1 = true
+                        }else {
+                            self.ans1 = false
+                        }
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20,style: .continuous)
+                                .fill(Color.yellow)
+                                .frame(width: 200, height: 100)
+                            Text(list[0])
+                        }
+                        NavigationLink(destination: QuizAnswerView(ans: self.ans1, phrase: self.now.OKans, sentence: self.now.correct, numB: numA), isActive: $showingDetail1) {
+                            EmptyView()
+                        }
+                    
+                    }
+                    
+                    Button(action: {
+                        self.showingDetail2.toggle()
+                        if self.list[1] == self.now.OKans {
+                            self.ans2 = true
+                        }else {
+                            self.ans2 = false
+                        }
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20,style: .continuous)
+                                .fill(Color.red)
+                                .frame(width: 200, height: 100)
+                            Text(list[1])
+                        }
+                        NavigationLink(destination:  QuizAnswerView(ans: self.ans2, phrase: self.now.OKans,sentence: self.now.correct, numB: numA), isActive: $showingDetail2) {
+                            EmptyView()
+                        }
+                    }
+                }
+            }.navigationBarHidden(true)
+       
         
     }
 }
 
-struct DetailView_Previews: PreviewProvider {
+
+struct QuizDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        QuizDetailView()
+        QuizDetailView(now: quizStore.quiz1[0], next: quizStore.quiz1[1])
     }
 }
--- Quiz Savvy/QuizRowView.swift
+++ Quiz Savvy/QuizRowView.swift
@@ -1,90 +1,28 @@
 //
-//  QuizRow.swift
+//  QuizRowView.swift
 //  Quiz Savvy
 //
-//  Created by 青木智大 on 2020/07/03.
+//  Created by 青木智大 on 2020/07/22.
 //  Copyright © 2020 青木智大. All rights reserved.
 //
 
 import SwiftUI
 
 struct QuizRowView: View {
-    @State var showingDetail1 = false
-    @State var showingDetail2 = false
-    @State var ans1: Bool = false
-    @State var ans2: Bool = false
-    var q: Quiz
-    @State var list: [String] = []
-    @State var showText = false
-    @State var Return: Bool = false
-    
+    var i: Quiz
     var body: some View {
-        NavigationView {
-            VStack {
-                Text(q.title)
-                Text(q.explain)
-                Spacer()
-                Button(action: {
-                    self.list = []
-                    self.list.append(self.q.OKans)
-                    self.list.append(self.q.BADans)
-                    self.list.shuffle()
-                    self.showText.toggle()
-                }) {
-                    Text("result")
-                }
-                if showText {
-                    Button(action: {
-                        self.showingDetail1.toggle()
-                        if self.list[0] == self.q.OKans {
-                            self.ans1 = true
-                        }else {
-                            self.ans1 = false
-                        }
-                    }) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 20,style: .continuous)
-                                .fill(Color.yellow)
-                                .frame(width: 200, height: 200)
-                            Text(list[0])
-                        }
-                    }.sheet(isPresented: $showingDetail1, onDismiss: { self.Return.toggle()}) {
-                        QuizAnswerView(ans: self.ans1, phrase: self.q.OKans,sentence: self.q.correct)
-                    }
-                    NavigationLink(destination: QuizDetailView(),isActive: $Return) {
-                        EmptyView()
-                    }
-                    
-                    Button(action: {
-                        self.showingDetail2.toggle()
-                        if self.list[1] == self.q.OKans {
-                            self.ans2 = true
-                        }else {
-                            self.ans2 = false
-                        }
-                    }) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 20,style: .continuous)
-                                .fill(Color.red)
-                                .frame(width: 200, height: 200)
-                            Text(list[1])
-                        }
-                }.sheet(isPresented: $showingDetail2, onDismiss: { self.Return.toggle()}) {
-                        QuizAnswerView(ans: self.ans2, phrase: self.q.OKans,
-                                       sentence: self.q.correct)
-                    }
-                    NavigationLink(destination: QuizDetailView(),isActive: $Return) {
-                        EmptyView()
-                    }
-                }
-            }
+        HStack {
+            Text(i.title)
         }
     }
 }
 
-
 struct QuizRowView_Previews: PreviewProvider {
     static var previews: some View {
-        QuizRowView(q: quizStore.quiz1[0])
+        Group {
+            QuizRowView(i: quizStore.quiz1[0])
+            QuizRowView(i: quizStore.quiz1[1])
+        }
+        .previewLayout(.fixed(width: 400, height: 80))
     }
 }
